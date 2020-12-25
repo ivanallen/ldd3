d4ad2d256bef05d97fb93e8b436452561536df20
allen <ivan_allen@163.com>
2020-12-25 22:05:28 +0800

    fix style

--- 14-scullpipe2/reader.c
+++ 14-scullpipe2/reader.c
@@ -7,7 +7,7 @@
 char buffer[4096];
 
 int main(int argc, char **argv) {
-	int delay = 1, n, m = 0;
+    int delay = 1, n, m = 0;
 
     int scullpipe = open("/dev/myscullpipe0", O_RDONLY);
 
@@ -15,24 +15,24 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-	fcntl(scullpipe, F_SETFL, fcntl(scullpipe, F_GETFL) | O_NONBLOCK);
+    fcntl(scullpipe, F_SETFL, fcntl(scullpipe, F_GETFL) | O_NONBLOCK);
 
-	while (1) {
-		n = read(scullpipe, buffer, 4096);
-		if (n >= 0) {
-			m = write(STDOUT_FILENO, buffer, n);
+    while (1) {
+        n = read(scullpipe, buffer, 4096);
+        if (n >= 0) {
+            m = write(STDOUT_FILENO, buffer, n);
         }
 
-		if ((n < 0 || m < 0) && (errno != EAGAIN)) {
-			break;
+        if ((n < 0 || m < 0) && (errno != EAGAIN)) {
+            break;
         }
 
         if (errno == EAGAIN) {
             perror("scullpipe0");
         }
 
-		sleep(delay);
-	}
+        sleep(delay);
+    }
 
     close(scullpipe);
 
